f7d3e9d61b979281d2e14114890011447de23366
HabibBehdarvandiDev <habibehdarvandi.dev@gmail.com>
2024-09-11 19:37:26 -0700

    Server Action  : create JWT functions Create/validate/decode

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { UserRegisterSchema } from "./schema";
+import prisma from "@/db/db";
+import bcrypt from "bcrypt";
 
 export async function POST(req: NextRequest) {
   let body;
@@ -14,10 +16,38 @@ export async function POST(req: NextRequest) {
 
   const validation = UserRegisterSchema.safeParse(body);
 
-  !validation.success &&
-    NextResponse.json(validation.error.formErrors.formErrors);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.formErrors.fieldErrors);
+  }
 
-    
+  // check user exist with username
+  try {
+    const isUserExist = await prisma.users.findUnique({
+      where: {
+        username: validation.data?.username,
+      },
+    });
 
-  return NextResponse.json(validation.data, { status: 200 });
+    if (isUserExist) {
+      return NextResponse.json(
+        { error: "user already exist !" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "errro while connecting to Database!" },
+      { status: 400 }
+    );
+  }
+
+  // hash the password
+  const hashed_password = await bcrypt.hash(
+    validation.data?.password,
+    parseInt(process.env.SALT_ROUND!) || 10
+  );
+
+  
+
+  return NextResponse.json(hashed_password, { status: 200 });
 }
---
+++ utils/cookies.ts
@@ -0,0 +1,54 @@
+import { SignJWT, jwtVerify, JWTPayload } from "jose";
+import { TextEncoder } from "util";
+
+// Define the secret key and algorithm for JWT
+const secretKey = new TextEncoder().encode(
+  process.env.JWT_SECRET! || "default_secret_key"
+);
+const issuer = process.env.APPLICATION_NAME! || "TGMAG";
+
+// Define expiration times for tokens
+const accessTokenExpiry = "1h"; // Access token valid for 1 hour
+
+// Create JWT (Access Token)
+export async function createJWT(payload: JWTPayload): Promise<string> {
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuer(issuer)
+    .setExpirationTime(accessTokenExpiry)
+    .sign(secretKey);
+
+  return token;
+}
+
+// Validate JWT (Check if it’s valid)
+export async function validateJWT(token: string): Promise<JWTPayload | null> {
+  try {
+    const { payload } = await jwtVerify(token, secretKey, {
+      issuer,
+    });
+    return payload;
+  } catch (error) {
+    console.error("JWT validation error:", error);
+    return null;
+  }
+}
+
+export async function decodeJWT(token: string): Promise<JWTPayload | null> {
+  try {
+    // Verify and decode the token
+    const { payload } = await jwtVerify(
+      token,
+      new TextEncoder().encode(secretKey.toString()),
+      {
+        algorithms: ["HS256"],
+      }
+    );
+
+    // Return the decoded payload
+    return payload;
+  } catch (error) {
+    console.error("Token validation error:", error);
+    return null;
+  }
+}
